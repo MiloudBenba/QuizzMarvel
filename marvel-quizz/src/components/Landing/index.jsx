45bc8afe8c04039a80b855ee6de5c276ed51dffb
Mimotest59 <myloud59@hotmail.fr>
2023-01-25 14:07:00 +0100

    feat: landing page initiated with buttons and background

--- marvel-quizz/src/components/Landing/index.jsx
+++ marvel-quizz/src/components/Landing/index.jsx
@@ -17,14 +17,35 @@ const Landing = () => {
     }, 1500);
   }, []);
 
+  // function setLeftImg onMouseOver
+  const setLeftImg = () => {
+    refWolverine.current.classList.add("leftImg");
+  };
+
+  // function setRightImg onMouseOver
+  const setRightImg = () => {
+    refWolverine.current.classList.add("rightImg");
+  };
+
+  // function onMouseOut
+  const clearImg = () => {
+    refWolverine.current.classList.remove("leftImg", "rightImg");
+  };
+
   // const for buttons with condition to appear after "startingImg" class removed
   const displayBtn = btn && (
     <>
-      <div className="leftBox">
+      <div onMouseOver={setLeftImg} onMouseOut={clearImg} className="leftBox">
         <button className="btn-welcome">Inscription</button>
       </div>
       <div className="rightBox">
-        <button className="btn-welcome">Connexion</button>
+        <button
+          onMouseOver={setRightImg}
+          onMouseOut={clearImg}
+          className="btn-welcome"
+        >
+          Connexion
+        </button>
       </div>
     </>
   );
