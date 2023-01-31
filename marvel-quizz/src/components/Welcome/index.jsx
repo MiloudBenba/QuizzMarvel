d350a718e56f558fec032de8dee6d8d7496d061d
Mimotest59 <myloud59@hotmail.fr>
2023-01-31 14:35:28 +0100

    feat: redirection to landing page if no connection feature

--- marvel-quizz/src/components/Welcome/index.jsx
+++ marvel-quizz/src/components/Welcome/index.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logout from "../LogOut/index";
 import Quiz from "../Quiz";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../Firebase/firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
-const Welcome = () => {
-  return (
+const Welcome = (props) => {
+  const [userSession, setUserSession] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let listener = onAuthStateChanged(auth, (user) => {
+      // @ts-ignore
+      user ? setUserSession(user) : navigate("/");
+    });
+    return () => {
+      listener();
+    };
+  }, [navigate]);
+
+  return userSession === null ? (
+    <>
+      <div className="loader"></div>
+      <p className="loaderText">Chargement...</p>
+    </>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />
